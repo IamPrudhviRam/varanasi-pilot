5beb230aecca927ac220e12ca3d17df2c5b2a082
IamPrudhviRam <maddinaprudhviram@gmail.com>
2022-12-20 10:54:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# varanasi-pilot
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/varanasi-pilot)
---
+++ app/app.component.ts
@@ -0,0 +1,209 @@
+import { Component } from '@angular/core';
+import { LatLngBounds } from '@agm/core';
+import { blueLatLngs } from '../constants/blue-lat-lngs';
+import { yellowLatLngs } from '../constants/yellow-lat-lngs';
+import { orangeLatLngs } from '../constants/orange-lat-lngs';
+import { default as varanasiAllGeojsons } from '../constants/varanasi_all.json';
+
+declare var google: any;
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  zoom: number = 8;
+
+  // initial center position for the map
+  latti: number = 25.3176;
+  longi: number = 82.9739;
+
+  distanceInMeters: number = 5000;
+
+  allGeojsons: any = varanasiAllGeojsons;
+  allZonePaths: any[] = [];
+  allTempProps: Array<any> = [];
+
+  blueLatLngs: any = blueLatLngs;
+  yellowLatLngs: any = yellowLatLngs;
+  orangeLatLngs: any = orangeLatLngs;
+
+  circleWhite = '#e6e6e6';
+  circleOrange = '#8cff66';
+  circleBlue = '#4da6ff'; // light #b3daff
+
+  strokeGrey = '#bfbfbf';
+
+  strokeYellow = '#e6e600';
+  strokeOrange = '#66ff33';
+  strokeBlue = '#b3daff';
+
+  strokeWeight = 1;
+  fillOpacity = 0.1;
+
+  hideTen: boolean = false;
+  showId: boolean = true;
+  icon: any = {
+    // url: 'https://img.icons8.com/color/24/null/map-pin.png',
+    // url: 'https://img.icons8.com/fluency/24/null/map-pin.png',
+    // url: 'https://img.icons8.com/fluency/' + 24 + '/000000/map-pin.png',
+    url: 'https://img.icons8.com/material-sharp/' + 24 + '/D12020/marker.png',
+
+    // url: 'https://img.icons8.com/color/' + 24 + '/000000/marker--v1.png',
+    // url: '',
+    scaledSize: {
+      width: 24,
+      height: 24,
+    },
+  };
+
+  ngOnInit(): void {
+    this.createGeoPath();
+    console.log('all paths:', this.allZonePaths);
+    console.log('all centers:', this.allTempProps);
+    console.log('currentTempType', this.currentTempType);
+  }
+
+  mapReady(map: any) {
+    const bonds: LatLngBounds = new google.maps.LatLngBounds();
+
+    bonds.extend(new google.maps.LatLng(this.latti, this.longi));
+
+    this.blueLatLngs.map((item: any) => {
+      bonds.extend(new google.maps.LatLng(item[0], item[1]));
+    });
+    this.yellowLatLngs.map((item: any) => {
+      bonds.extend(new google.maps.LatLng(item[0], item[1]));
+    });
+    this.orangeLatLngs.map((item: any) => {
+      bonds.extend(new google.maps.LatLng(item[0], item[1]));
+    });
+
+    map.fitBounds(bonds);
+  }
+
+  flattenArray(arr: Array<any>): number {
+    let counterIndex: number = 0;
+    while (arr.length == 1) {
+      arr = arr.flat(counterIndex++);
+    }
+    return counterIndex - 1;
+  }
+  createGeoPath() {
+    //multi-polygon
+    this.allGeojsons.features.map((feature: any) => {
+      let latLngArr: any[] = [];
+      let iterableArray = feature.geometry.coordinates.flat(
+        this.flattenArray(feature.geometry.coordinates)
+      );
+
+      iterableArray.map((item: any) => {
+        let latLng = {
+          lat: item[1],
+          lng: item[0],
+        };
+        latLngArr.push(latLng);
+      });
+
+      // Calculating avg center for polygon & adding zone for hover
+      this.allTempProps.push(
+        this.averageCenter(iterableArray, this.tooltipContent, this.baseUrl)
+      );
+
+      this.allZonePaths.push(latLngArr);
+    });
+  }
+
+  zoomChange(zoom: any) {
+    console.log('zoom change:', zoom);
+
+    let number = 24;
+    if (zoom <= 8) {
+      number = 36 - zoom;
+    } else {
+      number = 10 + 2 * zoom;
+    }
+
+    this.icon = {
+      // url: 'https://img.icons8.com/color/' + number + '/null/map-pin.png',
+      // url: 'https://img.icons8.com/fluency/' + number + '/null/map-pin.png',
+      // url: 'https://img.icons8.com/fluency/' + number + '/000000/map-pin.png',
+      // url: 'https://img.icons8.com/color/' + number + '/000000/marker--v1.png',
+      url:
+        'https://img.icons8.com/material-sharp/' +
+        number +
+        '/D12020/marker.png',
+
+      // url: '',
+      scaledSize: {
+        width: number,
+        height: number,
+      },
+    };
+  }
+
+  averageCenter(latLngArray: any[], tooltipContent: string, baseUrl: string) {
+    let latSum = 0;
+    let lngSum = 0;
+    let pathlength = latLngArray.length;
+    latLngArray.map((item: any[]) => {
+      latSum += item[0];
+      lngSum += item[1];
+    });
+    return [latSum / pathlength, lngSum / pathlength, tooltipContent, baseUrl];
+  }
+
+  polygonClicked(event: any, index: any) {
+    let url = this.allTempProps[index][3];
+    if (
+      this.allTempProps[index][2] === 'Sawah Sempadan' ||
+      (this.allTempProps[index][2] === 'BLOCK-W' &&
+        this.currentTempType == 'block') ||
+      this.currentTempType == 'lot'
+    )
+      this.redirect(url);
+    else
+      this.dialog.open(ConfirmDialogComponent, {
+        data: { type: 'wrong', message: 'No Data Available' },
+      });
+    // console.log('longi:', event.latLng.lat());
+  }
+
+  setMouseOver(index: any, infoWindow: any) {
+    console.log('setMouseOver');
+    this.isVisibleMarker = !this.isVisibleMarker;
+    this.hoveredNo = index;
+    this.latCenter = this.allTempProps[index][1];
+    this.lngCenter = this.allTempProps[index][0];
+    setTimeout(() => {
+      infoWindow.open();
+    });
+  }
+
+  setMouseOut(infoWindow: any) {
+    this.isVisibleMarker = !this.isVisibleMarker;
+    this.hoveredNo = null;
+    setTimeout(() => {
+      infoWindow.close();
+    });
+  }
+
+  circleClicked(event: any) {
+    console.log('clicked', event);
+  }
+  mouseOver(event: any) {
+    console.log('mouse over', event);
+  }
+  mouseOut(event: any) {
+    console.log('mouse over', event);
+  }
+
+  hideTenKms(event: any) {
+    this.hideTen = !this.hideTen;
+  }
+
+  showIds(event: any) {
+    this.showId = !this.showId;
+  }
+}
